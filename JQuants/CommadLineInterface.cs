c30ee532ced96f630651a90921f9bf4c47f663d6
larytet <larytet@21264bca-d741-11dd-8636-4158a08e8374>
2009-08-09 10:11:25 +0000

    Initial revision

---
+++ JQuants/CommadLineInterface.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+
+namespace JQuants {
+	
+	public class Command {
+		
+		public Command(string name, string shortDesctiption, string description, Action callback) {
+			Name = name;
+			ShortDescription = shortDesctiption;
+			Description = description;
+			Callback = callback;
+		}
+
+		public Command(string name, string shortDesctiption, string description) {
+			Name = name;
+			ShortDescription = shortDesctiption;
+			Description = description;
+			Callback = CallbackNotImplemented;
+		}
+		
+		public virtual bool IsCommand() {
+			return true;
+		}
+		
+		protected void CallbackNotImplemented() {
+		}
+
+		public string Name {
+			get;
+			protected set;
+		}
+		
+		public string ShortDescription {
+			get;
+			protected set;
+		}
+		
+		public string Description {
+			get;
+			protected set;
+		}
+		
+		public Action Callback {
+			get;
+			protected set;
+		}
+		
+	}
+	
+    public class Menu :Command{
+		public Menu(string name, string shortDesctiption, string description) 
+			:base(name, shortDesctiption, description) {
+			
+			Callback = DummyCommand;
+			Commands = new List<Command>();
+		}
+		
+		public Command AddCommand(string name, string shortDesctiption, string description, Action callback) {
+			Command cmd = new Command(name, shortDesctiption, description, callback);
+			Commands.Add(cmd);
+			return cmd;
+		}
+
+		public Menu AddMenu(string name, string shortDesctiption, string description, Action callback) {
+			Menu menu = new Menu(name, shortDesctiption, description);
+			Commands.Add(menu);
+			return menu;
+		}
+
+	    public List<Command> Commands {
+			get;
+			protected set;
+	    }
+
+		public virtual bool IsCommand() {
+			return false;
+		}
+
+		protected void DummyCommand() {
+		}
+		
+		public bool FindCommand(string name, out Command command) {
+			foreach ( Command cmd in Commands ) {
+				if (cmd.Name.Equals(name)) {
+					command = cmd;
+					return true;
+				}				
+			}
+			command = null;
+			return false;
+		}
+	}
+	
+	/// <summary>
+	/// Base class for a command line interface
+	/// </summary>
+	public abstract class CommandLineInterface {
+
+		public CommandLineInterface() {
+			ExitFlag = false;
+			RootMenu = new Menu("Main menu", "JQuants main menu", "Main menu provides access to the Logging, Trading\n"+ 
+	                                                        "and other main system moudles");
+			CurrentMenu = RootMenu;
+			SystemMenu = new Menu("Commnad line interface system menu", "", "");
+			SystemMenu.AddCommand("help", "List commands", "", PrintCommands);
+		}
+	
+		public abstract string Name { get; }
+		protected bool ExitFlag;
+	
+	
+		public Menu RootMenu {
+			get;
+			private set;
+		}
+	
+		public Menu SystemMenu {
+			get;
+			protected set;
+		}
+	
+		public Menu CurrentMenu { 
+			get;
+			protected set;
+		}
+	
+		protected void ProcessCommand(string cmdName) {
+			Action action = null;
+			Command cmd;
+				
+			if (cmdName.Equals("")) return;	
+				
+			// may be exit or help commands - always look in the system menu	
+			bool found = SystemMenu.FindCommand(cmdName, out cmd);
+			// not a system command try current menu then
+			if (!found) found = CurrentMenu.FindCommand(cmdName, out cmd);
+				
+			if (found && cmd.IsCommand()) {
+				cmd.Callback();
+			} else if (found && !cmd.IsCommand()) {
+				CurrentMenu = (Menu)cmd;
+				PrintCommands();
+			} else {
+				Console.WriteLine("Unknown command "+cmdName);
+			}
+		}
+	
+		protected void PrintCommands() {
+			if (CurrentMenu.Commands.Count != 0) {
+				int index = 0;
+				foreach ( Command cmd in CurrentMenu.Commands ) {
+					Console.WriteLine("{00} "+cmd.Name + " " + cmd.ShortDescription, index);
+				}
+			} else {
+				Console.WriteLine("No commands are available here");
+			}
+		}
+	
+		protected void PrintPrompt() {
+			Console.Write("$ ");
+		}
+	
+		protected void PrintTitle() {
+			Console.WriteLine(Name);
+			Console.WriteLine("=====================================");
+			Console.WriteLine("Type 'help' to get commands list");
+			Console.WriteLine("Type 'exit' to exit");
+		}
+
+	}
+}
