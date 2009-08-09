cf29842559e7c89280a3dd2afa7df68f085c040f
larytet <larytet@21264bca-d741-11dd-8636-4158a08e8374>
2009-08-09 10:11:25 +0000

    Initial revision

---
+++ trunk/JQuants/Main.cs
@@ -0,0 +1,65 @@
+//
+// To compile the example use line under Mono 2.4
+// /usr/bin/gmcs2 BaseConsole.cs Main.cs 
+//
+// This is a sample CLI application 
+	
+	
+	
+using System;
+
+namespace JQuants {
+	
+  class Program :JQuants.CommandLineInterface {   
+		
+	Program() {
+		LoadCommandLineInterface();
+	}
+	
+	public override string Name {  
+		get { return "JQuants"; }  
+	}  
+
+	private void SetExitFlag() {
+		ExitFlag = true;
+	}
+
+	protected void LoadCommandLineInterface() {  
+		SystemMenu.AddCommand("exit", "Exit from the program", "Clean and exit", this.SetExitFlag);
+	}  
+   
+		
+	public void Run() {
+
+		PrintTitle();
+
+		// While not exit command entered, process each command
+		while (true) {
+
+			PrintPrompt();
+			string input = Console.ReadLine();
+
+			if (input != "")
+			try {
+				// process command
+				ProcessCommand(input);
+				if (ExitFlag) break;
+
+			} catch (Exception ex) {
+				Console.WriteLine(ex.ToString());
+
+			} finally {
+				Console.WriteLine();
+			}
+		}
+
+	}
+		
+	static void Main(string[] args) {  
+		new Program().Run();  
+	}
+
+		
+  }
+	
+}
