c0007d6893211fa000ef9694860c173ba1d8c341
larytet <larytet@21264bca-d741-11dd-8636-4158a08e8374>
2009-08-10 07:09:50 +0000

    JQuant instead of JQuant

---
+++ trunk/JQuants/JQuant/CommadLineInterface.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+
+namespace JQuants {
+
+	public interface IWrite {
+		void WriteLine(string s);
+		void Write(string s);
+	}
+	
+	
+	public class Command {
+
+		public delegate void Callback(IWrite iWrite, string cmdName, object[] cmdArguments);
+
+		public Command(string name, string shortDesctiption, string description, Callback callback) {
+			Name = name;
+			ShortDescription = shortDesctiption;
+			Description = description;
+			Handler = callback;
+		}
+
+		public Command(string name, string shortDesctiption, string description) {
+			Name = name;
+			ShortDescription = shortDesctiption;
+			Description = description;
+			Handler = new Callback(Unsupported);
+		}
+		
+		void Unsupported(IWrite iWrite, string cmdName, object[] cmdArguments) {
+			iWrite.WriteLine("Unsupported command "+Name);
+		}
+		
+		public virtual bool IsCommand() {
+			return true;
+		}
+		
+		public string Name {
+			get;
+			protected set;
+		}
+		
+		public string ShortDescription {
+			get;
+			protected set;
+		}
+		
+		public string Description {
+			get;
+			protected set;
+		}
+		
+		public Callback Handler {
+			get;
+			protected set;
+		}
+		
+	}
+	
+    public class Menu :Command{
+		public Menu(string name, string shortDesctiption, string description) 
+			:base(name, shortDesctiption, description) {
+			Parent = this;
+			Handler = new Callback(DummyCommand);
+			Commands = new List<Command>();
+		}
+		
+		public Command AddCommand(string name, string shortDesctiption, string description, Callback callback) {
+			Command cmd = new Command(name, shortDesctiption, description, callback);
+			Commands.Add(cmd);
+			return cmd;
+		}
+
+		public Command AddCommand(Command command) {
+			Command cmd = command;
+			Commands.Add(cmd);
+			return cmd;
+		}
+
+		public Menu AddMenu(string name, string shortDesctiption, string description) {
+			Menu menu = new Menu(name, shortDesctiption, description);
+			Commands.Add(menu);
+			menu.Parent = this;
+			return menu;
+		}
+
+	    public List<Command> Commands {
+			get;
+			protected set;
+	    }
+		
+		public Menu Parent {
+			get;
+			set;
+		}
+		                    
+
+		public override bool IsCommand() {
+			return false;
+		}
+
+		protected void DummyCommand(IWrite iWrite, string cmdName, object[] cmdArguments) {
+			Console.WriteLine("Menu " + Name + 
+				": callback is called. Probably should be command");
+		}
+		
+		public bool FindCommand(string name, out Command command) {
+			foreach ( Command cmd in Commands ) {
+				if (cmd.Name.Equals(name)) {
+					command = cmd;
+					return true;
+				}				
+			}
+			command = null;
+			return false;
+		}
+	}
+	
+	public class CommandLineInterface {
+
+		public CommandLineInterface(string title) {
+			Name = title;
+			RootMenu = new Menu("Main menu", "JQuants main menu", "Main menu provides access to the Logging, Trading\n"+ 
+	                                                        "and other main system moudles");
+			CurrentMenu = RootMenu;
+			SystemMenu = new Menu("Commnad line interface system menu", "", "");
+			SystemMenu.AddCommand("help", "List commands", "", PrintCommands);
+			SystemMenu.AddCommand("..", "One level up", "", OneLevelUp);
+			SystemMenu.AddCommand("~", "Go to the main menu (root)", "", GotoRootMenu);
+		}
+	
+		public string Name { get; set;}
+	
+	
+		public Menu RootMenu {
+			get;
+			private set;
+		}
+	
+		public Menu SystemMenu {
+			get;
+			protected set;
+		}
+	
+		public Menu CurrentMenu { 
+			get;
+			protected set;
+		}
+			
+		public void ProcessCommand(IWrite iWrite, string cmdName) {
+			Command cmd;
+				
+			if (cmdName.Equals("")) return;	
+				
+			// may be exit or help commands - always look in the system menu	
+			bool found = SystemMenu.FindCommand(cmdName, out cmd);
+			// not a system command try current menu then
+			if (!found) found = CurrentMenu.FindCommand(cmdName, out cmd);
+				
+			if (found && cmd.IsCommand()) {
+				// temporary no parsing for the arguments
+				cmd.Handler(iWrite, cmdName, null);
+			} else if (found && !cmd.IsCommand()) {
+				CurrentMenu = (Menu)cmd;
+				PrintCommands(iWrite, "", null);
+			} else {
+				iWrite.WriteLine("Unknown command "+cmdName);
+			}
+		}
+		
+		private void OneLevelUp(IWrite iWrite, string cmdName, object[] cmdArguments) {
+			CurrentMenu = CurrentMenu.Parent;
+			PrintCommands(iWrite, "", null);
+		}
+
+		private void GotoRootMenu(IWrite iWrite, string cmdName, object[] cmdArguments) {
+			CurrentMenu = RootMenu;
+			PrintCommands(iWrite, "", null);
+		}
+
+		protected void PrintCommands(IWrite iWrite, string cmdName, object[] cmdArguments) {
+			PrintTitle(iWrite);
+			foreach ( Command cmd in CurrentMenu.Commands ) {
+				iWrite.WriteLine(cmd.Name + " - " + cmd.ShortDescription);
+			}
+			if (CurrentMenu.Commands.Count == 0) {
+				iWrite.WriteLine("No commands are available here");
+			}
+		}
+	
+		public void PrintPrompt(IWrite iWrite) {
+			iWrite.Write("$ ");
+		}
+	
+		public void PrintTitle(IWrite iWrite) {
+			iWrite.WriteLine(Name + " - " + CurrentMenu.Name);
+			iWrite.WriteLine("=====================================");
+			iWrite.WriteLine("help, exit, one level up - .., main menu - ~");
+			iWrite.WriteLine("");
+		}
+		
+	}
+}
---
+++ trunk/JQuants/JQuant/Main.cs
@@ -0,0 +1,104 @@
+//
+// To compile the example use line under Mono 2.4
+// /usr/bin/gmcs2 BaseConsole.cs Main.cs 
+//
+// This is a sample CLI application 
+	
+	
+	
+using System;
+
+namespace JQuants {
+	
+  class Program :JQuants.IWrite {   
+		
+	 Program() {
+		cli = new CommandLineInterface("JQuants");
+		LoadCommandLineInterface();
+	}
+		
+	private CommandLineInterface cli;
+	private bool ExitFlag;
+	
+
+	private void CleanupAndExit(IWrite iWrite, string cmdName, object[] cmdArguments) {
+		// chance to clean some things before exiting
+			
+		// flag to break out of the loop forever
+		ExitFlag = true;
+	}
+		
+	protected void menu1cmd1Callback(IWrite iWrite, string cmdName, object[] cmdArguments) {
+		iWrite.WriteLine("Menu 1 Command 1 called");
+	}
+
+	protected void menu1cmd2Callback(IWrite iWrite, string cmdName, object[] cmdArguments) {
+		iWrite.WriteLine("Menu 1 Command 2 called");
+	}
+
+	protected void menu2cmd1Callback(IWrite iWrite, string cmdName, object[] cmdArguments) {
+		iWrite.WriteLine("Menu 2 Command 1 called");
+	}
+
+	protected void menu2cmd2Callback(IWrite iWrite, string cmdName, object[] cmdArguments) {
+		iWrite.WriteLine("Menu 2 Command 2 called");
+	}
+
+	protected void LoadCommandLineInterface() {  
+		cli.SystemMenu.AddCommand("exit", "Exit from the program", "Cleanup and exit", this.CleanupAndExit);
+		Menu menu1 = cli.RootMenu.AddMenu("menu1", "Menu 1", "This is menu 1");
+		Menu menu2 = cli.RootMenu.AddMenu("menu2", "Menu 2", "This is menu 2");
+		menu1.AddCommand("cmd1", "Command 1", "This is command 1", menu1cmd1Callback);
+		menu1.AddCommand("cmd2", "Command 2", "This is command 2", menu1cmd2Callback);
+		menu2.AddCommand("cmd1", "Command 1", "This is command 1", menu2cmd1Callback);
+		menu2.AddCommand("cmd2", "Command 2", "This is command 2", menu2cmd2Callback);
+	}  
+
+	public void WriteLine(string s) {
+		Console.WriteLine(s);
+	}
+		
+	public void Write(string s) {
+		Console.Write(s);
+	}
+		
+	public void Run() {
+
+		cli.PrintTitle(this);
+
+		// While exit command not entered, process each command
+		while (true) {
+
+			cli.PrintPrompt(this);
+			string input = Console.ReadLine();
+
+			if (input != "")
+			try {
+				// process command - "this" is IWrite interface
+				cli.ProcessCommand(this, input);
+				if (ExitFlag) break;
+
+			} catch (Exception ex) {
+				WriteLine(ex.ToString());
+
+			} finally {
+				WriteLine("");
+			}
+		}
+			
+			
+		// last chance for the cleanup - close streams and so on
+
+	}
+		
+	static void Main(string[] args) {  
+		new Program().Run();  
+		
+		// very last chance for the cleanup - close streams and so on
+		// before i return control to the OS	
+	}
+
+		
+  }
+	
+}
