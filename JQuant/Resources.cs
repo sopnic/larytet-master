2b997772522bad956d2afb3ef93dd6c6e488effb
larytet <larytet@21264bca-d741-11dd-8636-4158a08e8374>
2009-08-21 16:48:57 +0000

    Update

--- JQuant/Resources.cs
+++ JQuant/Resources.cs
@@ -4,6 +4,40 @@ using System.Collections.Generic;
 
 namespace JQuant
 {
+
+	
+	/// <summary>
+	/// objects implement Mailbox
+	/// </summary>
+	public interface IMailbox {
+		/// <summary>
+		/// return name of the mailbox
+		/// </summary>
+		string GetName();
+		
+		/// <summary>
+		/// number of messages in the queue
+		/// </summary>
+		int GetCount();
+		
+		/// <summary>
+		/// maximum size recorded
+		/// </summary>
+		int GetMaxCount();
+		
+		/// <summary>
+		/// mailbox capacity
+		/// </summary>
+		int GetCapacity();
+		
+		int GetDropped();
+		int GetSent();
+		int GetReceived();
+		int GetTimeouts();
+	}
+	
+	
+	
 	/// <summary>
 	/// a storage of all created objects
 	/// an object central
