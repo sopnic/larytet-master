770f24df717228cab61c27c7de3ee247b5a7ba0c
larytet <larytet@21264bca-d741-11dd-8636-4158a08e8374>
2009-09-26 17:57:33 +0000

    TA namespace Proce of order is double and not integer

---
+++ trunk/JQuant/TA.cs
@@ -0,0 +1,25 @@
+
+using System;
+
+
+/// <summary>
+/// Recognition of basic TA patterns
+/// </summary>
+namespace TA
+{
+
+    public class PriceVolumeSeries
+    {
+        
+        System.Collections.Generic.List<double> price;
+        System.Collections.Generic.List<double> volume;
+    }
+    
+    public class Triangle
+    {
+        
+        public Triangle()
+        {
+        }
+    }
+}
