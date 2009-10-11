11d8104d2e9c11f4c27b2904148d3c0b7100ce8c
jerusalem.quant <jerusalem.quant@21264bca-d741-11dd-8636-4158a08e8374>
2009-10-11 22:57:19 +0000

    Missing callback for oper/stopstream added.

--- JQuant/FMRSim/Main_SetupCommandLine.cs
+++ JQuant/FMRSim/Main_SetupCommandLine.cs
@@ -200,6 +200,31 @@ namespace JQuant
             }
         }
 
+        protected void StopStreamCallBack(IWrite iWrite, string cmdName, object[] cmdArguments)
+        {
+            if (cmdArguments.Length == 1) iWrite.WriteLine("Please supply data type: MF | RZ | MDD");
+            else if (cmdArguments.Length > 2) iWrite.WriteLine("Too many arguments");
+            else
+            {
+                switch (cmdArguments[1].ToString().ToLower())
+                {
+                    case "mf":
+                        StopStream(iWrite, FMRShell.DataType.Maof);
+                        break;
+                    case "rz":
+                        StopStream(iWrite, FMRShell.DataType.Rezef);
+                        break;
+                    case "mdd":
+                        StopStream(iWrite, FMRShell.DataType.Madad);
+                        break;
+                    default:
+                        iWrite.WriteLine("Invalid data type parameter: " + cmdArguments[1].ToString());
+                        break;
+                }
+            }
+
+        }
+
         protected void StopStream(IWrite iWrite, FMRShell.DataType dt)
         {
             DataCollector.Stop(dt);
@@ -926,7 +951,7 @@ namespace JQuant
             menuOperations.AddCommand("StopLog", "Stop previosly started Maof Log - MF | MDD | RZ, to stop stream type Y",
                                   " Stop logger - Maof(MF) | Madad (MDD) | Rezef (RZ) and stream (Y/N). ", operStopLogCallback);
             menuOperations.AddCommand("StopStream", "Stop previosly started data stream - MF | MDD | RZ",
-                                  " Stop data stream - Maof(MF) | Madad (MDD) | Rezef (RZ) ", operStopLogCallback);
+                                  " Stop data stream - Maof(MF) | Madad (MDD) | Rezef (RZ) ", StopStreamCallBack);
 
 
             menuOperations.AddCommand("ShowLog", "Show existing loggers",
