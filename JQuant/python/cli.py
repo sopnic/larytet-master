6bd65ddc38c64e11ea5eea6a13db0e98422e90a3
larytet <larytet@21264bca-d741-11dd-8636-4158a08e8374>
2010-02-10 12:07:26 +0000

    Quit, Exit implemented

--- JQuant/python/cli.py
+++ JQuant/python/cli.py
@@ -7,9 +7,36 @@ class CliMain(cmd.Cmd):
     def __init__(self):
         cmd.Cmd.__init__(self)
         use_rawinput = 0
+
+    def help_help(self):
+        print "Display this help"
+
+    def do_quit(self, args):
+        self.do_exit(args)
+    def help_quit(self):
+        self.help_exit()
+
     def do_exit(self, args):
         exit()
-    
+    def help_exit(self):
+        print "Quit application"
+
+    def do_sleep(self, args):
+        time.sleep(float(args)/1000)
+    def help_sleep(self):
+        print "Sleep for specified number of milliseconds"
+
+
+    def do_none(self, args):
+        self._notimplemented()
+    def help_none(self, args):
+        print "Do nothing command"
+
+    def _notimplemented(self):
+        print 'Command not implemented'
+# endof class CliMain
+
+
 
 class ProcessCli(threading.Thread):
     def __init__(self):
@@ -19,6 +46,7 @@ class ProcessCli(threading.Thread):
         while (1):
             cmd = self.cliCmd.cmdloop()
             if cmd == 'exit': break
+# endof class ProcessCli
 
 
 print 'Create CLI'
