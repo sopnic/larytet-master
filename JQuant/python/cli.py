5fe7354bccef6db65952eb5581e048b79879092f
larytet <larytet@21264bca-d741-11dd-8636-4158a08e8374>
2010-02-10 11:55:41 +0000

    Command line loop works

--- JQuant/python/cli.py
+++ JQuant/python/cli.py
@@ -1,21 +1,29 @@
 #! /usr/bin/env python
 
+import threading, time, cmd
 
-import threading, time
+
+class CliMain(cmd.Cmd):
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        use_rawinput = 0
+    def do_exit(self, args):
+        exit()
+    
 
 class ProcessCli(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
+        self.cliCmd = CliMain()
     def run(self):
         while (1):
-            print 'Another sec'
-            time.sleep(1);
+            cmd = self.cliCmd.cmdloop()
+            if cmd == 'exit': break
 
 
 print 'Create CLI'
 processCli = ProcessCli()
 print 'Start CLI'
 processCli.start()
-print 'Wait for CLI to exit'
 processCli.join()    # Wait for the background task to finish
 
