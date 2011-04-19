3cd9a5e94baeb819435a4b7a986ff1b28192bc0b
larytet <larytet@21264bca-d741-11dd-8636-4158a08e8374>
2011-04-19 16:32:28 +0000

    More of IB wrapper

--- JQuant/IB/IB.cs
+++ JQuant/IB/IB.cs
@@ -6,5 +6,61 @@
 
 namespace IB
 {
+	public class Processor
+	{
+		/// <summary>
+		/// Initialize the class. This is a simpleton - only one object of this 
+		/// type exists in the system
+		/// </summary>
+		protected Processor()
+		{
+		
+		}
+		
+		/// <summary>
+		/// Create an instance of the processor
+		/// </summary>
+		/// <returns>
+		/// A <see cref="Processor"/>
+		/// </returns>
+		public Processor CreateProcessor ()
+		{
+			return null;
+		}
+		
+		/// <summary>
+		/// Connect to the server. This method will open a client socket and 
+		/// start the FSM which attempts to connect to the specified server. 
+		/// The actual connection is goingt to be established in the context of the 
+		/// Processor FSM. Notification onConnect can be received asynchronously 
+		/// by the application
+		/// </summary>
+		/// <param name="host">
+		/// A <see cref="System.String"/>
+		/// </param>
+		/// <param name="port">
+		/// A <see cref="System.Int32"/>
+		/// </param>
+		/// <returns>
+		/// A <see cref="System.Boolean"/>
+		/// </returns>
+		public bool Connect(string host, int port)
+		{
+			return true;
+		}
+		
+		/// <summary>
+		/// Returns true if connection is established with the server and 
+		/// the initial handshake is alright
+		/// </summary>
+		/// <returns>
+		/// A <see cref="System.Boolean"/>
+		/// </returns>
+		public bool isConnected()
+		{
+			return false;
+		}
+		
+	}
 
 } // namespace IB
