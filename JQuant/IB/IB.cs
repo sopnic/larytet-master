7c3e7aba733a1d06a5eb7b5ef6eea64706ed4243
larytet <larytet@21264bca-d741-11dd-8636-4158a08e8374>
2011-04-19 16:58:23 +0000

    More of IB

--- JQuant/IB/IB.cs
+++ JQuant/IB/IB.cs
@@ -4,11 +4,32 @@
 /// </summary>
 
 
+using System;
+
+// file system
+using System.IO;
+
+// text stream
+using System.Text;
+
 namespace IB
 {
+	/// <summary>
+	/// Message types to be sent to Processor
+	/// </summary>
 	public enum MessageType
 	{
 		Connect
+	}
+
+	/// <summary>
+	/// Possible states of the Processor
+	/// </summary>
+	public enum State
+	{
+		Idle,
+		Connecting,
+		Connected
 	}
 	
 	/// <summary>
@@ -16,8 +37,23 @@ namespace IB
 	/// </summary>
 	public class Message
 	{
-		MessageType id;
-	
+		public Message(MessageType id, object data, object data1)
+		{
+			this.data = data;
+			this.data1 = data1;
+			this.id = id;
+		}
+		
+		public Message(MessageType id, object data)
+		{
+			this.data = data;
+			this.data1 = null;
+			this.id = id;
+		}
+
+		public MessageType id;
+		public object data;
+		public object data1;
 	}
 	
 	public class Processor : JQuant.MailboxThread<Message>
@@ -29,6 +65,7 @@ namespace IB
 		protected Processor() 
 			:base("Processor", 100)
 		{
+			state = State.Idle;
 		
 		}
 		
@@ -49,8 +86,8 @@ namespace IB
 		/// The actual connection is goingt to be established in the context of the 
 		/// Processor FSM. Notification onConnect can be received asynchronously 
 		/// by the application
-		/// </summary>
 		/// <param name="host">
+		/// </summary>
 		/// A <see cref="System.String"/>
 		/// </param>
 		/// <param name="port">
@@ -61,6 +98,7 @@ namespace IB
 		/// </returns>
 		public bool Connect(string host, int port)
 		{
+			this.Send(new Message(MessageType.Connect, host, port));
 			return true;
 		}
 		
@@ -73,8 +111,83 @@ namespace IB
 		/// </returns>
 		public bool isConnected()
 		{
+			return (state == State.Connected);
+		}
+		
+		protected override void HandleMessage (Message message)
+		{
+			switch (state)
+			{
+			case State.Idle:
+				HandleMessage_Idle (message);
+				break;
+			case State.Connecting:
+				HandleMessage_Connecting (message);
+				break;
+			case State.Connected:
+				HandleMessage_Connected (message);
+				break;
+			}
+		}
+
+		protected void HandleMessage_Idle (Message message)
+		{
+			switch (message.id)
+			{
+				case MessageType.Connect:
+				HandleMessage_IdleConnect (message);
+				break;
+			}
+		
+		}
+		
+		protected void HandleMessage_Connected (Message message)
+		{
+			
+		}
+
+		protected void HandleMessage_Connecting (Message message)
+		{
+			
+		}
+		
+		protected void HandleMessage_IdleConnect (Message message)
+		{
+			string host = (string)message.data;
+			int port = (int)(message.data1);
+			clientSocket = null;
+			try
+			{
+				clientSocket = new System.Net.Sockets.TcpClient (host, port);
+			}
+			catch (Exception e)
+			{
+				System.Console.WriteLine ("Failed to connect " + host + ", port "+port);
+				System.Console.WriteLine (e.ToString ());
+			}
+			if (clientSocket != null)
+			{
+				bool res = writeToSocket(clientSocket, CLIENT_VERSION);
+				if (res)
+				{
+					this.state = State.Connecting;
+				}
+				else
+				{
+				}
+			}
+		
+		}
+
+		protected static bool writeToSocket(System.Net.Sockets.TcpClient clientSocket, int data)
+		{
 			return false;
 		}
+
+		protected State state;
+		protected System.Net.Sockets.TcpClient clientSocket;
+		private static int CLIENT_VERSION = 46;
+		private static int SERVER_VERSION = 38;
 		
 	}
 
