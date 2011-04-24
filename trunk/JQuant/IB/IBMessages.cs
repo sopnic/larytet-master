e3f7ddae6a72563939970b0d12b843e27396d184
larytet <larytet@21264bca-d741-11dd-8636-4158a08e8374>
2011-04-24 07:11:51 +0000

    More of IB

--- trunk/JQuant/IB/IBMessages.cs
+++ trunk/JQuant/IB/IBMessages.cs
@@ -37,10 +37,61 @@ namespace IB
 			state = RxHandler.State.Idle;
 		}
 	
+		/// <summary>
+		/// A message from IB is always starst from message identifier, for example REQ_MKT_DATA (1), followed by 
+		/// version (always 1?) and message specific data (tickerId). The information elemnets are zero delimited 
+		/// ASCII strings
+		/// </summary>
+		/// <param name="data">
+		/// A <see cref="System.Byte[]"/>
+		/// </param>
+		/// <param name="size">
+		/// A <see cref="System.Int32"/>
+		/// </param>
 		public void HandleData (byte[] data, int size)
 		{
+			switch (state)
+			{
+				case State.Idle:
+				HandleData_Idle(data, size);
+				break;
+				
+				case State.Processing:
+				HandleData_Processing(data, size);
+				break;
+				
+			}
+		}
+
+		/// <summary>
+		/// This is a new data - I expect to find a known message ID in the first information element
+		/// </summary>
+		/// <param name="data">
+		/// A <see cref="System.Byte[]"/>
+		/// </param>
+		/// <param name="size">
+		/// A <see cref="System.Int32"/>
+		/// </param>
+		public void HandleData_Idle (byte[] data, int size)
+		{
+		
 		}
 		
+		/// <summary>
+		/// I am in the middle of processing of a message and this is new chunk of data. I 
+		/// shall start the processing from the message ID I discovered in the previous chunk
+		/// </summary>
+		/// <param name="data">
+		/// A <see cref="System.Byte[]"/>
+		/// </param>
+		/// <param name="size">
+		/// A <see cref="System.Int32"/>
+		/// </param>
+		public void HandleData_Processing (byte[] data, int size)
+		{
+		
+		}
+				
 		protected RxHandlerCallback rxHandlerCallback;
 		protected RxHandler.State state;
 	}
