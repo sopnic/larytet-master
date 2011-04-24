140ba029ebe00b8da4013ff592e6665f106cf79b
larytet <larytet@21264bca-d741-11dd-8636-4158a08e8374>
2011-04-24 06:29:16 +0000

    IBMessages.cs added

---
+++ JQuant/IB/IBMessages.cs
@@ -0,0 +1,24 @@
+/// <summary>
+/// Contains class handling all received messages from the IB
+/// </summary>
+
+
+using System;
+
+// file system
+using System.IO;
+
+// text stream
+using System.Text;
+
+namespace IB
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class RxHandler
+	{
+	
+	}
+
+} // namespace IB
